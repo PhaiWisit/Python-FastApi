999ccd5a74c0f4a4614778e770267b6f49a2fd20
Wisit Yoosabai <87866405+PhaiWisit@users.noreply.github.com>
2023-12-24 02:03:06 +0700

    auth

--- database/models.py
+++ database/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import  Column, Integer, String, DateTime
+from sqlalchemy import  Column, Integer, String, DateTime,Boolean
 from sqlalchemy.sql import func
 from database.database import Base
+import datetime
+
 
 class Applicant(Base):
     __tablename__ = 'applicants'
@@ -13,3 +15,17 @@ class Applicant(Base):
     phone = Column(String(256))
     expectedSalary = Column(Integer)
     createdDate = Column(DateTime, default=func.now(), nullable=False)
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer,primary_key=True)
+    username = Column(String(50))
+    password = Column(String(100))
+    
+class TokenTable(Base):
+    __tablename__ = "token"
+    user_id = Column(Integer)
+    access_toke = Column(String(450), primary_key=True)
+    refresh_toke = Column(String(450),nullable=False)
+    status = Column(Boolean)
+    created_date = Column(DateTime, default=datetime.datetime.now)    
